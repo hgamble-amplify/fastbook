d00d8c685e37bc48f9f22387e2dd693dce362300
Harrison Gamble <hgamble@amplify.com>
2022-04-13 15:01:17 -0400

    add install for graphviz

--- setup_venv.sh
+++ setup_venv.sh
@@ -3,5 +3,6 @@ source dl_learning/bin/activate
 python3 -m pip install jupyterlab
 python3 -m pip install install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 python3 -m pip install fastbook
+python3 -m pip install fastai
+python3 -m pip install graphviz
 python3 -m ipykernel install --user --name=dl_learning
-deactivate
